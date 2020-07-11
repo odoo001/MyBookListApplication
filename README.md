40a351706e2c8ca0333039c0a855c5df25cbe40d
odoo001 <40703730+odoo001@users.noreply.github.com>
2020-07-11 11:05:46 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyBookListApplication
+ Apl task
---
+++ app/src/main/java/com/example/mybooklistapplication/spUtil.java
@@ -0,0 +1,49 @@
+package com.example.mybooklistapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+
+public class spUtil {
+    private spUtil() {}
+    public static final String PERF_NAME = "BooksPreferences";
+    public static final String POSITIVE = "position";
+    public static final String QUERY = "query";
+
+    public static SharedPreferences getPrefs(Context context) {
+        return context.getSharedPreferences(PERF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static String getPreferenceString(Context context, String key) {
+        return getPrefs(context).getString(key, "");
+    }
+
+    public static int getPreferenceInt (Context context, String key) {
+        return getPrefs(context).getInt(key, 0);
+    }
+
+    public static void setPreferenceString(Context context, String key, String value) {
+        SharedPreferences.Editor editor = getPrefs(context).edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static void setPreferenceInt(Context context, String key, int value) {
+        SharedPreferences.Editor editor = getPrefs(context).edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    public static ArrayList<String> getQueryList(Context context) {
+        ArrayList<String> queryList = new ArrayList<String>();
+        for (int i = 1; i<=5; i++) {
+            String query = getPrefs(context).getString(QUERY + String.valueOf(i), "");
+            if (!query.isEmpty()) {
+                query = query.replace(",","");
+                queryList.add(query.trim());
+            }
+        }
+        return queryList;
+    }
+}
